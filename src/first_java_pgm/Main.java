50c760aa9fd33a1af705a6bc9b60ebf1df1eecd0
ADITYA PATEL <adityapatel8071@gmail.com>
2025-05-08 18:30:50 +0530

    initial commit

---
+++ src/first_java_pgm/Main.java
@@ -0,0 +1,9 @@
+package first_java_pgm;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.print(args[2]);
+	}
+}
+
+//execute it through command line 'java Main.java 10 20 30 40 50' o/p: 30
---
+++ src/first_java_pgm/Main1.java
@@ -0,0 +1,10 @@
+package first_java_pgm;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
